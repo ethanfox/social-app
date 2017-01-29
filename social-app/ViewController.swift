0463bf906268e26980a5802607a10bb4175b518d
Ethan Fox <ethanj.fx@gmail.com>
2017-01-29 18:41:03 -0500

    Initial Commit

---
+++ social-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  social-app
+//
+//  Created by Ethan Fox on 1/29/17.
+//  Copyright © 2017 Ethan Fox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
