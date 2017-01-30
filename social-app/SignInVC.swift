9f22022dd3749339867a200b6bbf8415058f1864
Ethan Fox <ethanj.fx@gmail.com>
2017-01-30 10:22:32 -0500

    Email Sign In

--- social-app/SignInVC.swift
+++ social-app/SignInVC.swift
@@ -13,6 +13,10 @@ import Firebase
 
 
 class SignInVC: UIViewController {
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -62,8 +66,37 @@ class SignInVC: UIViewController {
             }
         })
         
-        
     }
+    
+    
+    
+    @IBAction func signInBtnTapped(_ sender: Any) {
+        
+        if let email = emailField.text, let password = passwordField.text {
+            
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error == nil {
+                    
+                    print("Email User Authenticated with Firebase - Prepare for Sign In ")
+                    
+                } else {
+                    
+                    FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if error != nil {
+                            
+                            print("FAILED: Unable to Authenticate with Firebase Email")
+                        } else {
+                            
+                            print("SUCCESS: User Authenticated with Firebase Email")
+                        }
+                    })
+                    
+                }
+            })
+            
+        }
+    }
+    
 
 }
 
